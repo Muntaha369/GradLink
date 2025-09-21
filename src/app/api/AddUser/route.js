c7e7e7e1b109ad89289fe139583c8317bf831bc0
Muntaha <muntahasayed396@gmail.com>
2025-09-21 13:42:00 +0530

    Add user api updated

--- src/app/api/AddUser/route.js
+++ src/app/api/AddUser/route.js
@@ -1,47 +1,61 @@
-import { NextResponse } from "next/server";
-import ConnectDB from "../../db/db";
-import User from "../../model/model";
-import Admin from "../../model/adminModel";
+import { writeFile } from 'fs/promises';
+import { NextResponse } from 'next/server';
+import path from 'path';
+import ConnectDB from '../../db/db'
+import User from '../../model/model'
 
 export async function POST(req) {
   try {
-    await ConnectDB();
 
-    const body = await req.json();
-    const { name, email, pass, phone, GY, Uname, JobDesc } = body;
+    await ConnectDB()
+
+    const data = await req.formData();
+    const file = data.get('file');
+    const name = data.get('name');
+    const Uname = data.get('Uname');
+    const email = data.get('email');
+    const pass = data.get('pass');
+    const phone = data.get('phone');
+    const GY = data.get('GY');
+    const JobDesc = data.get('JobDesc');
 
     
-    if (!email) {
-      return NextResponse.json(
-        { message: "Email is required" },
-        { status: 400 }
-      );
+    if (!file) {
+      return NextResponse.json({ success: false, message: 'No file found' }, { status: 400 });
     }
-      
-      const findUser = await User.findOne({ email });
-
-      if (findUser) {
-        return NextResponse.json({
-          msg: "User with this email already exists"
-        }, { status: 409 });
-      }
-
-      const NewUser = await User.create({ name, email, pass, phone, GY, Uname, JobDesc });
-
-      return NextResponse.json({
-        msg: "User created successfully",
-        user: NewUser,
-      }, { status: 201 });
+    if (!email || !Uname || !name || !pass || !phone || !GY || !JobDesc) {
+      return NextResponse.json({ success: false, message: 'Image type (imgtype) is required' }, { status: 400 });
     }
 
-    catch (error) {
-    
-    console.error("API Error:", error);
+    const bytes = await file.arrayBuffer();
+    const buffer = Buffer.from(bytes);
 
     
-    return NextResponse.json(
-      { message: "An internal server error occurred" },
-      { status: 500 }
-    );
+    const filename = `${Date.now()}-${Uname}`; 
+    const filePath = path.join(process.cwd(), 'public', filename+".png");
+    await writeFile(filePath, buffer);
+
+    const newUser = await User.create({
+      name,
+      email,
+      pass,
+      phone,
+      GY,
+      Uname,
+      JobDesc
+    })
+
+    
+    console.log(`File uploaded: ${filename}`);
+
+    return NextResponse.json({
+      success: true,
+      message: 'File uploaded successfully',
+      User:newUser
+    });
+
+  } catch (error) {
+    console.error('Error handling upload:', error);
+    return NextResponse.json({ success: false, message: 'Server error' }, { status: 500 });
   }
 }
